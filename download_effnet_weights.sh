5db8d968fb5c5b11f4552fa2b5d3f01c584e1eac
Chris <christoph.stoeckl@hotmail.com>
2020-07-31 13:48:30 +0200

    added test_effnet_spikes.sh

--- download_effnet_weights.sh
+++ download_effnet_weights.sh
@@ -1,3 +1,7 @@
 wget https://storage.googleapis.com/cloud-tpu-checkpoints/efficientnet/randaug/efficientnet-b7-randaug.tar.gz
 
+mkdir -p checkpoints/imagenet/effnet-b7
 tar -xf efficientnet-b7-randaug.tar.gz -C checkpoints/imagenet/effnet-b7/
+rm efficientnet-b7-randaug.tar.gz
+
+mv checkpoints/imagenet/effnet-b7/efficientnet-b7-randaug/* checkpoints/imagenet/effnet-b7
